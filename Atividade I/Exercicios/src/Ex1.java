2eee5c5585af0e3fb416318948806d0ffa060a3f
eduardogd09 <eduardoguldavila@gmail.com>
2025-03-16 18:12:08 -0300

    Ex1

---
+++ Atividade I/Exercicios/src/Ex1.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex1 { 
+    public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o tamanho da lista: ");
+        int tamanho = sc.nextInt();
+        boolean x = true;
+        for (int i = 0; i < tamanho; i++){
+            System.out.println("Digite  o " + (i+1) + "º número da lista");
+            lista.add(sc.nextInt());
+
+        }
+        sc.close();
+        for(int i = 0; i < lista.size() - 1; i++){
+            if (lista.get(i) == lista.get(i+1)){
+                System.out.println("O número repetido é: " + lista.get(i));
+                x = false  ;
+                break;
+            }
+            
+            
+        }
+        if (x == true){
+            System.out.println("Não há números repetidos");
+
+        }
+
+    }
+}
